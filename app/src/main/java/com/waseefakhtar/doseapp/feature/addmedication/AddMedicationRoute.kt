1917b31798852ae90f0afccd7d9b1937804970be
Waseef Akhtar <waseefakhtar@gmail.com>
2022-06-20 00:23:35 +0500

    Implement add button

--- app/src/main/java/com/waseefakhtar/doseapp/feature/addmedication/AddMedicationRoute.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/feature/addmedication/AddMedicationRoute.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -19,6 +21,8 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -28,6 +32,7 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -38,6 +43,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -261,6 +267,23 @@ fun AddMedicationScreen(onBackClicked: () -> Unit) {
                 },
                 label = { Text(text = TimesOfDay.Night.name)  },
                 selectedIcon = { Icon(imageVector = Icons.Default.Done, contentDescription = "Selected") }
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(4.dp))
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(0.75F)
+                .height(56.dp)
+                .align(Alignment.CenterHorizontally),
+            onClick = {
+
+            },
+            shape = MaterialTheme.shapes.extraLarge
+            ) {
+            Text(
+                text = stringResource(id = R.string.done),
+                style = MaterialTheme.typography.bodyLarge
             )
         }
     }
