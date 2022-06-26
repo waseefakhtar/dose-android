cc0b34f9af4366e6676beb5baf348deef16eaedd
Waseef Akhtar <waseefakhtar@gmail.com>
2022-06-26 19:10:31 +0500

    Add DailyOverview card

--- app/src/main/java/com/waseefakhtar/doseapp/feature/home/HomeScreen.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/feature/home/HomeScreen.kt
@@ -1,11 +1,21 @@
 package com.waseefakhtar.doseapp.feature.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.cardColors
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeRoute(
@@ -17,11 +27,16 @@ fun HomeRoute(
 
 @Composable
 fun HomeScreen() {
-    Greeting("Android")
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Greeting()
+        DailyOverview()
+    }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting() {
     Column {
         // TODO: Add greeting based on time of day e.g. Good Morning, Good Afternoon, Good evening.
         // TODO: Get name from DB and show user's first name.
@@ -35,4 +50,25 @@ fun Greeting(name: String) {
             style = MaterialTheme.typography.displayMedium
         )
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DailyOverview() {
+
+    Spacer(modifier = Modifier.padding(8.dp))
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp),
+        shape = RoundedCornerShape(36.dp),
+        colors = cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer ,
+            contentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    ) {
+
+
+    }
+
 }
