170763e7af915c9f1b534b4319abdecb26e9eeb6
Alvi <alviand03@gmail.com>
2022-10-07 10:02:21 +0700

    add toast when success insert new medication to db

--- app/src/main/java/com/waseefakhtar/doseapp/feature/addmedication/AddMedicationRoute.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/feature/addmedication/AddMedicationRoute.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.waseefakhtar.doseapp.R
 import com.waseefakhtar.doseapp.domain.model.Medication
@@ -59,6 +60,12 @@ import kotlinx.coroutines.launch
 import java.text.DateFormatSymbols
 import java.util.Calendar
 import java.util.Date
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AddMedicationRoute(onBackClicked = {}, navigateToMedicationConfirm = {})
+}
 
 @Composable
 fun AddMedicationRoute(
--- app/src/main/java/com/waseefakhtar/doseapp/feature/medicationconfirm/viewmodel/MedicationConfirmViewModel.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/feature/medicationconfirm/viewmodel/MedicationConfirmViewModel.kt
@@ -4,6 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.waseefakhtar.doseapp.feature.medicationconfirm.usecase.AddMedicationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -12,10 +15,13 @@ class MedicationConfirmViewModel @Inject constructor(
     private val addMedicationUseCase: AddMedicationUseCase
 ) : ViewModel() {
 
+    private val _isMedicationSaved = MutableSharedFlow<Long>()
+    val isMedicationSaved = _isMedicationSaved.asSharedFlow()
+
     fun addMedication(state: MedicationConfirmState) {
         viewModelScope.launch {
             val medication = state.medication
-            addMedicationUseCase.addMedication(medication)
+            _isMedicationSaved.emit(addMedicationUseCase.addMedication(medication))
         }
     }
 }
