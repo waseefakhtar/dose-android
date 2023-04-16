d1ae02957d80f5b91fbc31cf883caa867fdefdb8
Waseef Akhtar <waseefakhtar@gmail.com>
2023-04-17 00:08:26 +0500

    Update date based on TimeOfDay, get time remaining in hours and minutes

--- app/src/main/java/com/waseefakhtar/doseapp/feature/home/HomeScreen.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/feature/home/HomeScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -176,10 +175,7 @@ fun MedicationCard(medication: Medication, viewModel: HomeViewModel) {
                 )
                 Spacer(modifier = Modifier.height(16.dp))
                 Text(
-                    text = stringResource(
-                        id = R.string.time_remaining,
-                        getTimeRemaining(medication)
-                    ),
+                    text = getTimeRemaining(medication),
                     fontWeight = FontWeight.Bold
                 )
             }
--- app/src/main/java/com/waseefakhtar/doseapp/util/TimeUtils.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/util/TimeUtils.kt
@@ -1,18 +1,32 @@
 package com.waseefakhtar.doseapp.util
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.waseefakhtar.doseapp.R
 import com.waseefakhtar.doseapp.domain.model.Medication
 import com.waseefakhtar.doseapp.extension.toFormattedString
+import java.util.Calendar
 import java.util.concurrent.TimeUnit
 import kotlin.math.abs
 
+@Composable
+fun getTimeRemaining(medication: Medication): String {
+    val currentTime = Calendar.getInstance().time
+    val dateBefore = medication.date
+    val timeDiff = abs(currentTime.time - dateBefore.time)
 
-fun getTimeRemaining(medication: Medication): Long {
-    return if (medication.date.toFormattedString() == medication.endDate.toFormattedString()) {
-        0L
-    } else {
-        val dateBeforeInMs: Long = medication.date.time
-        val dateAfterInMs: Long = medication.endDate.time
-        val timeDiff = abs(dateAfterInMs - dateBeforeInMs)
-        (TimeUnit.DAYS.convert(timeDiff, TimeUnit.MILLISECONDS) + 1L)
+    // If the medication is scheduled for a future date, display days remaining
+    if (medication.date.toFormattedString() != medication.endDate.toFormattedString()) {
+        val daysRemaining = TimeUnit.DAYS.convert(timeDiff, TimeUnit.MILLISECONDS) + 1L
+        return stringResource(id = R.string.time_remaining, daysRemaining, stringResource(id = R.string.days))
+    }
+
+    // If the medication is scheduled for today, calculate time remaining in hours and minutes
+    val hoursRemaining = TimeUnit.HOURS.convert(timeDiff, TimeUnit.MILLISECONDS)
+    val minutesRemaining = TimeUnit.MINUTES.convert(timeDiff, TimeUnit.MILLISECONDS)
+    return when {
+        hoursRemaining > 1 -> stringResource(id = R.string.time_remaining, hoursRemaining, stringResource(id = R.string.hours))
+        minutesRemaining > 1 -> stringResource(id = R.string.time_remaining, minutesRemaining, stringResource(id = R.string.days))
+        else -> stringResource(id = R.string.take_dose_now)
     }
 }
