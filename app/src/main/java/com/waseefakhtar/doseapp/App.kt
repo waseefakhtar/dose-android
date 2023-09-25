590e5f7c3bd51eb80a0df63c2e2f9f21d9cc4a57
Waseef Akhtar <waseefakhtar@gmail.com>
2023-09-26 01:00:23 +0200

    Create notification channel

--- app/src/main/java/com/waseefakhtar/doseapp/App.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/App.kt
@@ -1,7 +1,31 @@
 package com.waseefakhtar.doseapp
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                MedicationNotificationService.MEDICATION_CHANNEL_ID,
+                "Medication Reminder",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel.description = "Notifications for medication reminder"
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
---
+++ app/src/main/java/com/waseefakhtar/doseapp/MedicationNotificationService.kt
@@ -0,0 +1,8 @@
+package com.waseefakhtar.doseapp
+
+class MedicationNotificationService {
+
+    companion object {
+        const val MEDICATION_CHANNEL_ID = "medication_channel"
+    }
+}
