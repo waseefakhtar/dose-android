9fd365b9e35140bafdfed2d3857bc95df49e66f1
Waseef Akhtar <waseefakhtar@gmail.com>
2023-09-26 01:39:59 +0200

    Add medication reminder notification

--- app/src/main/java/com/waseefakhtar/doseapp/MedicationNotificationService.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/MedicationNotificationService.kt
@@ -1,6 +1,49 @@
 package com.waseefakhtar.doseapp
 
-class MedicationNotificationService {
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+class MedicationNotificationService(
+    private val context: Context
+) {
+    private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+    fun showNotification() {
+        val activityIntent = Intent(context, MainActivity::class.java)
+        val activityPendingIntent = PendingIntent.getActivity(
+            context,
+            1,
+            activityIntent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+        )
+
+        val receiverIntent = Intent(context, MedicationNotificationReceiver::class.java)
+        val takenPendingIntent = PendingIntent.getBroadcast(
+            context,
+            2,
+            receiverIntent,
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+        )
+
+        // TODO: Add action.
+        val notification = NotificationCompat.Builder(context, MEDICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_dose)
+            .setContentTitle("Medication Reminder")
+            .setContentText("It is time to take your medication. Open the app to log it.")
+            .setContentIntent(activityPendingIntent)
+            /*.addAction(
+                R.drawable.doctor,
+                "Take now",
+                takenPendingIntent)*/
+            .build()
+
+        // TODO: Use medication id as notification id.
+        notificationManager.notify(1, notification)
+    }
 
     companion object {
         const val MEDICATION_CHANNEL_ID = "medication_channel"
