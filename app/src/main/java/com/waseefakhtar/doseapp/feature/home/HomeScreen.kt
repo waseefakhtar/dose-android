c8aee0a633ca8e56f4fa9812dbd612eced6e5c2f
Wottrich <lucas.wottrich@ifood.com.br>
2023-10-10 08:43:03 -0300

    Fix take medication button behaviour

--- app/src/main/java/com/waseefakhtar/doseapp/feature/home/HomeScreen.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/feature/home/HomeScreen.kt
@@ -273,7 +273,13 @@ fun DailyMedications(navController: NavController, analyticsHelper: AnalyticsHel
                         )
                     }
                     is MedicationListItem.MedicationItem -> {
-                        MedicationCard(it.medication, viewModel, analyticsHelper)
+                        MedicationCard(
+                            it.medication,
+                            onTakeButtonClicked = { medication ->
+                                analyticsHelper.logEvent(AnalyticsEvents.TAKE_MEDICATION_CLICKED)
+                                viewModel.takeMedication(medication)
+                            }
+                        )
                     }
                 }
             }
@@ -285,72 +291,6 @@ sealed class MedicationListItem {
     data class OverviewItem(val medicationsToday: List<Medication>, val isMedicationListEmpty: Boolean) : MedicationListItem()
     data class MedicationItem(val medication: Medication) : MedicationListItem()
     data class HeaderItem(val headerText: String) : MedicationListItem()
-}
-
-@Composable
-fun MedicationCard(medication: Medication, viewModel: HomeViewModel, analyticsHelper: AnalyticsHelper) {
-
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp),
-        shape = RoundedCornerShape(30.dp),
-        colors = cardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceVariant,
-        )
-    ) {
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-
-            Column(
-                modifier = Modifier
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.Start
-            ) {
-                Text(
-                    text = medication.name,
-                    fontWeight = FontWeight.Bold,
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Text(
-                    text = medication.timesOfDay.joinToString(", ")
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Text(
-                    text = getTimeRemaining(medication),
-                    fontWeight = FontWeight.Bold
-                )
-            }
-
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                horizontalAlignment = Alignment.End
-            ) {
-
-                Button(
-                    onClick = {
-                        analyticsHelper.logEvent(AnalyticsEvents.TAKE_MEDICATION_CLICKED)
-                        viewModel.takeMedication(medication)
-                    },
-                    enabled = !medication.medicationTaken
-                ) {
-                    if (medication.medicationTaken) {
-                        Text(
-                            text = "Taken"
-                        )
-                    } else {
-                        Text(
-                            text = "Take now"
-                        )
-                    }
-                }
-            }
-        }
-    }
 }
 
 @OptIn(ExperimentalPermissionsApi::class)
---
+++ app/src/main/java/com/waseefakhtar/doseapp/feature/home/MedicationCard.kt
@@ -0,0 +1,121 @@
+package com.waseefakhtar.doseapp.feature.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.waseefakhtar.doseapp.domain.model.Medication
+import com.waseefakhtar.doseapp.util.getTimeRemaining
+import java.util.Date
+
+@Composable
+fun MedicationCard(
+    medication: Medication,
+    onTakeButtonClicked: (Medication) -> Unit
+) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        shape = RoundedCornerShape(30.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+        )
+    ) {
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .weight(2f)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.Start
+            ) {
+                Text(
+                    text = medication.name,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.titleLarge
+                )
+                Text(
+                    text = medication.timesOfDay.joinToString(", ")
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = getTimeRemaining(medication),
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            Button(
+                modifier = Modifier.weight(1f).padding(end = 16.dp),
+                onClick = { onTakeButtonClicked(medication) },
+                enabled = !medication.medicationTaken
+            ) {
+                if (medication.medicationTaken) {
+                    Text(
+                        text = "Taken"
+                    )
+                } else {
+                    Text(
+                        text = "Take now"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun MedicationCardTakeNowPreview() {
+    MedicationCard(
+        Medication(
+            id = 123L,
+            name = "A big big name for a little medication I needs to take",
+            dosage = 1,
+            recurrence = "2",
+            endDate = Date(),
+            timesOfDay = listOf(),
+            medicationTaken = false,
+            date = Date(),
+        )
+    ) {
+
+    }
+}
+
+@Preview
+@Composable
+private fun MedicationCardTakenPreview() {
+    MedicationCard(
+        Medication(
+            id = 123L,
+            name = "A big big name for a little medication I needs to take",
+            dosage = 1,
+            recurrence = "2",
+            endDate = Date(),
+            timesOfDay = listOf(),
+            medicationTaken = true,
+            date = Date(),
+        )
+    ) {
+
+    }
+}
