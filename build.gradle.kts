72095085574050c26274952503ed5eac55dba312
Ankur <ankuranand141295@gmail.com>
2023-12-16 10:20:29 +0530

    fixed the lint warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins{
+plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
