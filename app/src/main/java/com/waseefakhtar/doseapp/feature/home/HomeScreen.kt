729979d31562e03ac8406a5e96b78401c23da7f9
GitGitro <108683123+GitGitro@users.noreply.github.com>
2023-12-21 19:13:20 +0100

    Fix crash on A14, closes #116

--- app/src/main/java/com/waseefakhtar/doseapp/feature/home/HomeScreen.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/feature/home/HomeScreen.kt
@@ -1,7 +1,10 @@
 package com.waseefakhtar.doseapp.feature.home
 
 import android.Manifest
+import android.app.AlarmManager
+import android.content.Intent
 import android.os.Build
+import android.provider.Settings
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -42,6 +45,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -66,12 +70,14 @@ import java.util.Date
 fun HomeRoute(
     navController: NavController,
     askNotificationPermission: Boolean,
+    askAlarmPermission: Boolean,
     navigateToMedicationDetail: (Medication) -> Unit,
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
     val analyticsHelper = AnalyticsHelper.getInstance(LocalContext.current)
     val state = viewModel.state
+    PermissionAlarmDialog(analyticsHelper, askAlarmPermission)
     PermissionDialog(analyticsHelper, askNotificationPermission)
     HomeScreen(navController, analyticsHelper, state, viewModel, navigateToMedicationDetail)
 }
@@ -458,6 +464,64 @@ fun PermissionDialog(analyticsHelper: AnalyticsHelper, askNotificationPermission
                                     notificationPermissionState.launchPermissionRequest()
                                     openAlertDialog.value = false
                                     analyticsHelper.logEvent(AnalyticsEvents.NOTIFICATION_PERMISSION_DIALOG_ALLOW_CLICKED)
+                                }
+                            ) {
+                                Text(stringResource(R.string.allow))
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun PermissionAlarmDialog(analyticsHelper: AnalyticsHelper, askAlarmPermission: Boolean) {
+    val context = LocalContext.current
+    val alarmManager = ContextCompat.getSystemService(context, AlarmManager::class.java)
+    if (askAlarmPermission && (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE)) {
+        val alarmPermissionState = rememberPermissionState(Manifest.permission.SCHEDULE_EXACT_ALARM) { isGranted ->
+            when (isGranted) {
+                true -> analyticsHelper.logEvent(AnalyticsEvents.ALARM_PERMISSION_GRANTED)
+                false -> analyticsHelper.logEvent(AnalyticsEvents.ALARM_PERMISSION_REFUSED)
+            }
+        }
+        if (alarmManager?.canScheduleExactAlarms() == false) {
+            val openAlertDialog = remember { mutableStateOf(true) }
+
+            when {
+                openAlertDialog.value -> {
+                    analyticsHelper.logEvent(AnalyticsEvents.ALARM_PERMISSION_DIALOG_SHOWN)
+
+                    AlertDialog(
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.Notifications,
+                                contentDescription = stringResource(R.string.alarms)
+                            )
+                        },
+                        title = {
+                            Text(text = stringResource(R.string.alarms_permission_required))
+                        },
+                        text = {
+                            Text(text = stringResource(R.string.alarms_permission_required_description_message))
+                        },
+                        onDismissRequest = {
+                            openAlertDialog.value = false
+                            analyticsHelper.logEvent(AnalyticsEvents.ALARM_PERMISSION_DIALOG_DISMISSED)
+                        },
+                        confirmButton = {
+                            Button(
+                                onClick = {
+                                    Intent().also { intent ->
+                                        intent.action = Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM
+                                        context.startActivity(intent)
+                                    }
+
+                                    openAlertDialog.value = false
+                                    analyticsHelper.logEvent(AnalyticsEvents.ALARM_PERMISSION_DIALOG_ALLOW_CLICKED)
                                 }
                             ) {
                                 Text(stringResource(R.string.allow))
