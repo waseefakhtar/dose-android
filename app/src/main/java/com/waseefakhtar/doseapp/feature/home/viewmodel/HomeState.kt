841b0f5cf3e09a6d33eb36fa335305d3309e1277
Waseef Akhtar <waseefakhtar@gmail.com>
2025-01-04 23:17:06 +0100

    Use StateFlow instead of mutableStateOf
    Using StateFlow to:
    1. Combines all state updates into a single stream
    2. Provides a proper initial value
    3. Removes the need for separate state management

--- app/src/main/java/com/waseefakhtar/doseapp/feature/home/viewmodel/HomeState.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/feature/home/viewmodel/HomeState.kt
@@ -1,12 +1,10 @@
 package com.waseefakhtar.doseapp.feature.home.viewmodel
 
 import com.waseefakhtar.doseapp.domain.model.Medication
-import com.waseefakhtar.doseapp.extension.toFormattedYearMonthDateString
-import java.util.Date
 
 data class HomeState(
     val greeting: String = "",
     val userName: String = "",
-    val lastSelectedDate: String = Date().toFormattedYearMonthDateString(),
+    val lastSelectedDate: String,
     val medications: List<Medication> = emptyList()
 )
