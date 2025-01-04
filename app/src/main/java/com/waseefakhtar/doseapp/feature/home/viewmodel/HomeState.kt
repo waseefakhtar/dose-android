0e22bbda0136b2ff95adf6d89ae957e3ff1eaed6
Waseef Akhtar <waseefakhtar@gmail.com>
2025-01-04 22:53:33 +0100

    Fix parsing empty lastSelectedDate

--- app/src/main/java/com/waseefakhtar/doseapp/feature/home/viewmodel/HomeState.kt
+++ app/src/main/java/com/waseefakhtar/doseapp/feature/home/viewmodel/HomeState.kt
@@ -1,10 +1,12 @@
 package com.waseefakhtar.doseapp.feature.home.viewmodel
 
 import com.waseefakhtar.doseapp.domain.model.Medication
+import com.waseefakhtar.doseapp.extension.toFormattedYearMonthDateString
+import java.util.Date
 
 data class HomeState(
     val greeting: String = "",
     val userName: String = "",
-    val lastSelectedDate: String = "",
+    val lastSelectedDate: String = Date().toFormattedYearMonthDateString(),
     val medications: List<Medication> = emptyList()
 )
